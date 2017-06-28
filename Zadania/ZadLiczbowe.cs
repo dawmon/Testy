04e117c4f462edfdbc3282bbcab7a8454e80b9fd
TheSt <TheSt@DESKTOP-OGJMHAG>
2017-06-28 22:59:31 +0200

    Losowanie liczb?

--- Zadania/ZadLiczbowe.cs
+++ Zadania/ZadLiczbowe.cs
@@ -8,14 +8,33 @@ namespace Zadania
 {
     class ZadLiczbowe : Zadanie
     {
-         protected override bool sprPyt(string[] poprOdp)
+        /*  protected override bool sprPyt(string[] poprOdp)
+         {
+             if (this.odp == poprOdp)
+                 return true;    
+             else
+                 return false;
+         }*/
+        Random Losowaliczba = new Random();
+        int liczba1;
+        int liczba2;
+        public void liczby {
+            liczba1 = Losowaliczba.Next(51);
+            liczba2 = Losowaliczba.Next(51);
+        }
+        private bool odpowiedzi()
         {
-            if (this.odp == poprOdp)
+            if ((liczba1 + liczba2 == suma.Value)
+                 (liczba1 - liczba2 == minus.Value)
+                 (liczba1 * liczba2 == mno.Value)
+                 (liczba1 / liczba2 == dzie.Value))
                 return true;
             else
                 return false;
         }
-    static int Znak()   //sprawdzamy czy wpisana odpowiedz w liczbowym jest cyfrą   
+
+
+        static int Znak()   //sprawdzamy czy wpisana odpowiedz w liczbowym jest cyfrą   
         {
             int odp;    
             while (!int.TryParse (Console.ReadLine (), out odp))  //fosujemy użycie cyfry 
