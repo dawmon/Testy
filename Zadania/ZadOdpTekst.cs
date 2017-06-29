bdda3e404767b967f0a155251b030c8d825e0c61
TheSt <TheSt@DESKTOP-OGJMHAG>
2017-06-29 08:07:42 +0200

    O co tu chodzi?

--- Zadania/ZadOdpTekst.cs
+++ Zadania/ZadOdpTekst.cs
@@ -8,6 +8,32 @@ namespace Zadania
 {
     class ZadOdpTekst : Zadanie
     {
+        protected string odp;
+        public Porownaj(string odp, string odpo)
+        {
+         
+        }
+        public string odpo
+        {
+            get { return odp; }
+            set { odp = value; }
+        }
+        private string odp;
+        public static int Porownaj(
+        
+        bool ignoreCase
+         )  
+
+        public ZadOdpTekst(string tresc, string[] odpo)
+        {
+            pyt = tresc;
+            if (this.odp == odpo)
+            {
+                return true;
+                    else
+                return false;
+            }
+        }
 
     }
 }
